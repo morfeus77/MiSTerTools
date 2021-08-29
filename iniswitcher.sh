feba01ae461d8986b969d9eff6cde6ac5112150b
morfeus77 <maartenotten77@gmail.com>
2021-08-29 07:26:26 +0200

    Backup and delete feature added

--- iniswitcher.sh
+++ iniswitcher.sh
@@ -4,6 +4,7 @@ import curses
 import glob
 import shutil
 from  curses import panel
+from datetime import datetime
 
 class Menu(object):
     def __init__(self, items, stdscreen):
@@ -15,8 +16,8 @@ class Menu(object):
 
         self.position = 0
         self.items = items
+        self.items.append(("Create MiSTer.ini Backup", createbackup))
         self.items.append(("Exit and reboot", "Exit"))
-
 
     def navigate(self, n):
         self.position += n
@@ -80,34 +81,54 @@ class Menu(object):
             self.window.refresh()
             curses.doupdate()
 
+            if os.path.isfile('/media/fat/.activeprofile'):
+               with open('/media/fat/.activeprofile', "r") as activeprofiles:
+                     activeprofile = activeprofiles.readline()
+            else:
+               activeprofile = "Not Set"
+
             for index, item in enumerate(self.items):
                 if index == self.position:
                     mode = curses.A_REVERSE
                 else:
                     mode = curses.A_NORMAL
                 itemlist = item[0].split("/")
-                msg = "%d. %s" % (index, itemlist[-1])
+                if item[0] == activeprofile:
+                  self.window.attron(curses.color_pair(1))
+                  msg = "%d. %s (currently active)" % (index, itemlist[-1])
+                  self.window.attroff(curses.color_pair(1))
+                else:
+                  msg = "%d. %s" % (index, itemlist[-1])
                 self.window.addstr(1 + index, 1, msg, mode)
 
-            statusbarstr = "Press ENTER to use ini as MiSTer.ini (main)"
+            statusbarstr = "ENTER to apply ini, D to delete an ini"
 
-            if os.path.isfile('/media/fat/.activeprofile'):
-               with open('/media/fat/.activeprofile', "r") as activeprofiles:
-                     activeprofile = activeprofiles.readline()
-            else:
-               activeprofile = "Not Set"
             self.window.attron(curses.color_pair(3))
             self.window.addstr(height-3, 1, "%s - Active profile: %s" % (statusbarstr,activeprofile))
             self.window.attroff(curses.color_pair(3))
 
             key = self.window.getch()
+            if ((key == 100) or (key == 68)) and self.position < len(self.items) - 2:
+               deleteini(self.items[self.position][0])
+               self.items.remove(self.items[self.position])
+               self.window.clear()                
+               self.window.addstr(4 + index, 1, "%s removed" % self.items[self.position][0], mode)
+
             if key in [curses.KEY_ENTER, ord("\n")]:
                 if self.position == len(self.items) - 1:
                     break
                 else:
-                    activateini(self.items[self.position][0])
-                    self.window.clear()
-                    self.window.addstr(3 + index, 1, "%s is now active..." % self.items[self.position][0], mode)
+                    if self.position == len(self.items) - 2:
+                      backupfilename = createbackup()
+                      self.window.clear()
+                      self.window.addstr(4 + index, 1, "MiSTer.ini backed up as %s" % backupfilename, mode)
+                      if not backupfilename in self.items:
+                         self.items.append((backupfilename, curses.beep))
+                         self.items = sorted(self.items)
+                    else:
+                      activateini(self.items[self.position][0])
+                      self.window.clear()
+                      self.window.addstr(3 + index, 1, "%s is now active..." % self.items[self.position][0], mode)
             else:
                 if key == curses.KEY_UP:
                    self.navigate(-1)
@@ -135,9 +156,7 @@ class IniProfileSwitcher(object):
 
       main_menu = Menu(sorted(main_menu_items), self.screen)
       main_menu.display()
-
       
-
 def main():
     curses.wrapper(IniProfileSwitcher)
     curses.echo(0)
@@ -150,6 +169,16 @@ def activateini(inifilename):
     shutil.copyfile(inifilename, '/media/fat/MiSTer.ini')
     with open('/media/fat/.activeprofile', "w") as activeprofile:
          activeprofile.write(inifilename)
+
+def createbackup():
+    dateTimeObj = datetime.now()
+    date = dateTimeObj.date()
+    backupname = '/media/fat/MiSTerBackup%s.ini' % date
+    shutil.copyfile('/media/fat/MiSTer.ini',backupname)
+    return(backupname)
+
+def deleteini(inifilename):
+    os.remove(inifilename)
 
 if __name__ == "__main__":
     main()
